2fa342375aa2d0f9836fd988b7d4dff0769fc721
aperezNWO <57961103+aperezNWO@users.noreply.github.com>
2022-11-24 23:51:39 -0500

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 //
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 //
 @Component({
   selector: 'app-cart',
@@ -12,8 +13,20 @@ export class CartComponent {
   //
   items = this.cartService.getItems();
   //
-  constructor(private cartService: CartService) 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+  //
+  constructor(private cartService: CartService,private formBuilder: FormBuilder) 
   { 
       //
+  }
+  //
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 }
